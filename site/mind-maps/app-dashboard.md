7d3d308a0c9d0fcdcc2a0b7c1078c9428053ba6d
Brian Sunter <bsunter@gmail.com>
2019-11-29 01:39:20 -0800

    phone map
    Phone map

---
+++ site/mind-maps/app-dashboard.md
@@ -0,0 +1,4 @@
+---
+mindmap_url: /mind-maps/test.mm
+layout: layouts/mind-map
+---
---
+++ site/mind-maps/app-dashboard.mm
@@ -0,0 +1,11 @@
+<map version="1.0.0"><node ID="723D4244-D09E-40B5-8A9A-03E313036952" BACKGROUND_COLOR="#FFFFFF" TEXT="Appdash" COLOR="#4B4B4B" POSITION="right" STYLE="bubble"><edge COLOR="#4B4B4B" WIDTH="4" /><font NAME="Helvetica" SIZE="24" /><node ID="CAE6B745-C389-4632-90D6-AE0DF84AFDF9" TEXT="Tag cloud all reviews " COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#FF9559" WIDTH="6" /><font NAME="Helvetica" SIZE="20" /><node ID="3FD1E477-FFBB-4170-B833-F36A10E2B965" TEXT="Bigrams" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#FF9559" WIDTH="5" /><font NAME="Helvetica" SIZE="20" /></node>
+<node ID="1FF575CE-E161-4A9C-A6F8-9131321A6341" TEXT="Trigrams" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#FF9559" WIDTH="5" /><font NAME="Helvetica" SIZE="20" /></node>
+<node ID="E416826F-E79C-4041-B294-99FF560E8EC2" TEXT="Word freq" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#FF9559" WIDTH="5" /><font NAME="Helvetica" SIZE="20" /></node>
+</node>
+<node ID="4D805FC9-22CB-4E47-9131-F4AE211EB651" TEXT="New words in version" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#72C8FF" WIDTH="6" /><font NAME="Helvetica" SIZE="20" /></node>
+<node ID="8A6748B3-3EB7-4863-B448-BC67DAE88D09" TEXT="Over time" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#AF4FC8" WIDTH="6" /><font NAME="Helvetica" SIZE="20" /><node ID="FDE5E8BD-0141-47DB-8720-7DC9FBC6B515" TEXT="Reviews " COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#AF4FC8" WIDTH="5" /><font NAME="Helvetica" SIZE="20" /></node>
+<node ID="E35E6310-C1DE-4C15-8B4B-6EB5B7D0E2D2" TEXT="Sentiment" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#AF4FC8" WIDTH="5" /><font NAME="Helvetica" SIZE="20" /></node>
+</node>
+<node ID="C50B86CF-7F57-4C87-B28D-4D432B287C16" TEXT="Support tickets" COLOR="#6D6D6D" POSITION="right" STYLE="fork"><edge COLOR="#FFCD3B" WIDTH="6" /><font NAME="Helvetica" SIZE="20" /></node>
+</node>
+</map>
