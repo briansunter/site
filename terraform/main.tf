d1e451dd089ae084b95080bc1c69501f3a0e537d
Brian Sunter <public@briansunter.com>
2022-02-16 20:54:57 -0800

    basic terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,14 +17,23 @@ module "site" {
   domain = var.domain
   zone_id = var.zone_id
 }
+module "dev_site" {
+  source = "./site"
+  region = var.region
+  domain = "dev.${var.domain}"
+  zone_id = var.zone_id
+}
 
 module "pipeline" {
   source = "./pipeline"
   region = var.region
   bucket_name = module.site.bucket_name
+  bucket_dev_name = module.dev_site.bucket_name
   cloudfront_distribution_id = module.site.cloudfront_distribution_id
+  cloudfront_distribution_dev_id = module.dev_site.cloudfront_distribution_id
   app_name = var.app_name
   git_repository_owner = var.git_repository_owner
   git_repository_name = var.git_repository_name
   git_repository_branch = var.git_repository_branch
 }
+
